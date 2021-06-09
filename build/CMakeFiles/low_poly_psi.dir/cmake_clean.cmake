f0fa2d4bef0cd7f4d582523f87351be0ab452e11
private-panda <85609059+private-panda@users.noreply.github.com>
2021-06-09 17:45:11 +0800

    Add files via upload

---
+++ build/CMakeFiles/low_poly_psi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/low_poly_psi.dir/home/mingli/Desktop/PolyPSI/low_poly_psi.cpp.o"
+  "low_poly_psi"
+  "low_poly_psi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/low_poly_psi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/psi_plain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/psi_plain.dir/home/mingli/Documents/SEAL/native/examples/PolyPSI/psi_plain.cpp.o"
+  "psi_plain"
+  "psi_plain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/psi_plain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
